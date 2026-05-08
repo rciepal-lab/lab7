87e17f6f9385576890c4b4a255dfa1a6f2193814
rciepal-lab <rciepal@student.agh.edu.pl>
2026-05-08 13:56:51 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
+import os
+from dotenv import load_dotenv
 
+# Load variables from .env file
+load_dotenv()
+
+# Read variables
+name = os.getenv("NAME")
+city = os.getenv("CITY")
+debug = os.getenv("DEBUG")
+
+print("Hello!")
+print(f"Name: {name}")
+print(f"City: {city}")
+print(f"Debug: {debug}")
